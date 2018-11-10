41dac1275565cf8179536fe06ae4ea482d800518
Aluno <aluno@Alunos-MacBook-Pro.local>
2018-11-09 20:09:54 -0400

    Subindo nosso pjt

---
+++ matriz_nomesobrenome/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace matriz_nomesobrenome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qtdLinhas = 2, qtdColunas = 2;
+            string[,] m = new string[qtdLinhas, qtdColunas];
+
+            string conteudo = "";
+
+            for (int i = 0; i < qtdLinhas; i++)
+            {
+                for (int j = 0; j < qtdColunas; j++)
+                {
+                    if (j == 0) Console.WriteLine("Informe o nome: ");
+                    if (j == 1) Console.WriteLine("Informe o sobrenome: ");
+
+                    m[i, j] = Console.ReadLine();
+                    conteudo += m[i, j] + " ";
+                }
+                conteudo += "\n";
+            }
+            Console.WriteLine(conteudo);
+        }
+    }
+}
